c74b464f4be3e3aa2889522995a1d7d7c7078221
Hiroki Mori <yamori813@yahoo.co.jp>
2025-04-13 07:01:22 +0900

    more modify for LINUX

--- bl08.c
+++ bl08.c
@@ -44,13 +44,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <termios.h>
 
 #ifdef LINUX
 // Terrible hack here because POSIX says 'ioctl()' is in <stropts.h> but
 // for example Mac OS X Tiger does not have this header, OTOH I think
 // that <sys/ictl.h> is not POSIX either so how do you write
 // actual POSIX compliant code that compiles cleanly on POSIX...
-#include <termios.h>
 extern int ioctl (int filedes, int command, ...);
 // End of hack
 #endif
